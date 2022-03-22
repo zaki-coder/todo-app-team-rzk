a2046a82d641751a84755079f5c6e9134fda997c
zaki-coder <zakiazizi1841992@gmail.com>
2022-03-22 11:30:35 +0100

    styling sidebar

---
+++ todo-project/src/components/Sidebar.jsx
@@ -0,0 +1,39 @@
+import React from "react";
+import homeLogo from "../assets/Vector.png";
+import folderLogo from "../assets/folder.png";
+import settingsLogo from "../assets/settings.png";
+import logoutLogo from "../assets/vector2.png";
+
+const Sidebar = () => {
+  return (
+    <div className="flex h-screen min-w-[316px] w-[316px] p-14  border-r-[1px]  ">
+      <div className="">
+        <h2 className="font-bold text-xl mb-[82px]">Todo-app</h2>
+        <div className="flex-col ">
+          <div className="flex ">
+            <img className="mr-4 mb-14" src={homeLogo} />
+            <h4>Dashboard</h4>
+          </div>
+
+          <div className="flex ">
+            <img className="mr-4" src={folderLogo} />
+            <h4>Overview</h4>
+          </div>
+        </div>
+        <div className="">
+          <div className="flex ">
+            <img className="mr-4 mb-14" src={settingsLogo} />
+            <h4>Settings</h4>
+          </div>
+
+          <div className="flex ">
+            <img className="mr-4" src={logoutLogo} />
+            <h4>Log out</h4>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Sidebar;
---
+++ todo-project/tailwind.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    colors: {
+      whiteSmoke: "#FEFEFE",
+    },
+    extend: {},
+  },
+  plugins: [],
+};
