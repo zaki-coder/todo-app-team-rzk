d5fa59fb82853655d42fc493b1eced11c3ef3126
zaki-coder <zakiazizi1841992@gmail.com>
2022-03-23 15:54:58 +0100

    work on context

--- todo-project/src/components/Button.jsx
+++ todo-project/src/components/Button.jsx
@@ -1,9 +1,9 @@
-import React from "react";
+
 
 const Button = () => {
   return (
     <div className="flex justify-center items-center bg-paleBlue2 rounded-lg my-2 space-x-1 p-2 cursor-pointer hover:bg-green">
-      <button className="text-myGreen text-3xl  ">+</button>
+      <button type="button" className="text-myGreen text-3xl">+</button>
     </div>
   );
 };
